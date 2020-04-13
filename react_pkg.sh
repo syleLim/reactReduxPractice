31842df9d444b3cf2d3950036e5c561759bc569c
syleLim <syle6492@gmail.com>
2020-04-13 02:11:33 -0700

    delete al

--- react_pkg.sh
+++
@@ -1,6 +0,0 @@
-npm install react react-dom
-npm install --save-dev webpack webpack-dev-server webpack-cli html-webpack-plugin
-npm install --save-dev @babel/core babel-loader @babel/preset-react @babel/preset-env
-
-# css setting
-npm install --save-dev styled-components
