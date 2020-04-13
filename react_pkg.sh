5fd4813d2a90b1f58f6e93a5219095f1f0fdc0c4
syleLim <syle6492@gmail.com>
2020-04-13 01:59:58 -0700

    default set

---
+++ react_pkg.sh
@@ -0,0 +1,6 @@
+npm install react react-dom
+npm install --save-dev webpack webpack-dev-server webpack-cli html-webpack-plugin
+npm install --save-dev @babel/core babel-loader @babel/preset-react @babel/preset-env
+
+# css setting
+npm install --save-dev styled-components
